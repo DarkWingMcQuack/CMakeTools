91ab57de3b6d03a56c31f66c179ff776edf1aa00
DarkWingMcQuack <np-hardass@protonmail.com>
2018-11-20 00:45:21 +0100

    fixed clang-format

--- clang-format.cmake
+++ clang-format.cmake
@@ -1,12 +1,12 @@
 # additional target to perform clang-format run, requires clang-format
 
 # get all project files
-file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+file (GLOB_RECURSE SOURCE_FILES "src/*.cpp" "include/*.hpp")
 
 add_custom_target(
   clangformat
   COMMAND clang-format
   -style=file
   -i
-  ${ALL_SOURCE_FILES}
+  ${SOURCE_FILES}
   )
